e9f45fec898e742a5f0fba006036138ae9264ccd
renan <renansilvaytb@gmail.com>
2022-07-07 17:44:36 -0300

    elements

--- Stage05/refatorando-desafio02/js/elements.js
+++ Stage05/refatorando-desafio02/js/elements.js
@@ -1,17 +1,22 @@
-const buttonCards = document.querySelector('.cardButton')
 const buttonPlay = document.querySelector('.buttonPlay')
 const buttonPause = document.querySelector('.buttonPause')
 const buttonStop = document.querySelector('.buttonStop')
+// increase and decrease minutes
 const buttonLess = document.querySelector('.buttonLess')
 const buttonMost = document.querySelector('.buttonMost')
+// theme
 const buttonLight = document.querySelector('.lightTheme')
 const buttonDark = document.querySelector('.darkTheme')
-
+// minutes and seconds
 const minutesHtml = document.querySelector('.minutes')
 const secondsHtml = document.querySelector('.seconds')
+// sounds
+const buttonForest = document.querySelector('.forest')
+const buttonRain = document.querySelector('.rain')
+const buttonFire = document.querySelector('.coffee-shop')
+const buttonCoffee = document.querySelector('.fire')
 
 export {
-  buttonCards,
   buttonPlay,
   buttonPause,
   buttonStop,
@@ -20,5 +25,9 @@ export {
   buttonLight,
   buttonDark,
   minutesHtml,
-  secondsHtml
+  secondsHtml,
+  buttonForest,
+  buttonRain,
+  buttonFire,
+  buttonCoffee
 }
