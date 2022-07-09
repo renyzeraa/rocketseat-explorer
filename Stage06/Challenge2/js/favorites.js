6a91086cd32de8fe57e514b87b8ff701e43475b8
renan <renansilvaytb@gmail.com>
2022-07-08 23:00:35 -0300

    deploy final

--- Stage06/Challenge2/js/favorites.js
+++ Stage06/Challenge2/js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers
-      }))
-  }
-}
+import { GithubUser } from './githubuser.js'
 
 export class Favorites {
   constructor(root) {
@@ -98,6 +85,7 @@ export class FavoritesView extends Favorites {
         '.user img'
       ).src = `https://www.github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://www.github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
