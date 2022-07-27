ff6df72929e9e97a62d28965afe118372939afa8
renan <renansilvaytb@gmail.com>
2022-07-27 09:56:48 -0300

    request and params

--- Stage08/Aula01/src/server.js
+++ Stage08/Aula01/src/server.js
@@ -2,5 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params
+  response.send(`Id da mensagem: ${id} com o usuario ${user}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
