60767b17574db9ea32c9372b33101bbe1f9e50ac
renan <renansilvaytb@gmail.com>
2022-07-27 11:39:26 -0300

    app error

--- Stage08/Aula01/src/server.js
+++ Stage08/Aula01/src/server.js
@@ -1,3 +1,7 @@
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
+
 const express = require('express')
 
 const routes = require('./routes')
@@ -6,6 +10,22 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 const PORT = 3333
 
