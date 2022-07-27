245de9628c2b1f6fb1e4c006d9aa0c46ce5c32f2
renan <renansilvaytb@gmail.com>
2022-07-27 11:23:18 -0300

    how to use middlewares

--- Stage08/Aula01/src/routes/user.routes.js
+++ Stage08/Aula01/src/routes/user.routes.js
@@ -4,14 +4,8 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next) {
-  console.log('Voce passou pelo middleware')
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post('/', myMiddleware, usersController.create)
+usersRoutes.post('/', usersController.create)
 
 module.exports = usersRoutes
