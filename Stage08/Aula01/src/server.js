72c1d2ac40c56e000b68b71dbfee4fd0b17ffa10
renan <renansilvaytb@gmail.com>
2022-07-27 10:09:13 -0300

    request query

--- Stage08/Aula01/src/server.js
+++ Stage08/Aula01/src/server.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 
 const app = express()
@@ -7,5 +8,12 @@ app.get('/message/:id/:user', (request, response) => {
   response.send(`Id da mensagem: ${id} com o usuario ${user}`)
 })
 
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Pagina: ${page}, mostrar: ${limit}`)
+})
+
 const PORT = 3333
+
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
