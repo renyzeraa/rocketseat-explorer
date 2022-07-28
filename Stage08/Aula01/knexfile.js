986500bb31769805138468cd1597cef3ff9305d9
renan <renansilvaytb@gmail.com>
2022-07-28 10:30:14 -0300

    knex config

---
+++ Stage08/Aula01/knexfile.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+    },
+
+    useNullAsDefault: true
+  }
+}
