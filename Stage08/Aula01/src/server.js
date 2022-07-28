f7e09677b4ed917c7eb4021babf8f4494539ae13
renan <renansilvaytb@gmail.com>
2022-07-28 09:03:11 -0300

    import sqlite3

--- Stage08/Aula01/src/server.js
+++ Stage08/Aula01/src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors')
-
+const database = require('./database')
 const AppError = require('./utils/AppError')
 
 const express = require('express')
@@ -10,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
