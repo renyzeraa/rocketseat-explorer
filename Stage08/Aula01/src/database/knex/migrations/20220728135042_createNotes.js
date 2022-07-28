a6e57d99b51d5fa32714c6c16545e89fbae93fe2
renan <renansilvaytb@gmail.com>
2022-07-28 10:57:15 -0300

    migration create notes

--- Stage08/Aula01/src/database/knex/migrations/20220728135042_createNotes.js
+++ Stage08/Aula01/src/database/knex/migrations/20220728135042_createNotes.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = knex =>
+  knex.schema.createTable('notes', table => {
+    table.increments('id')
+    table.text('title')
+    table.text('descriptions')
+    table.integer('user_id').references('id').inTable('users')
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+    table.timestamp('created_at').default(knex.fn.now())
+    table.timestamp('updated_at').default(knex.fn.now())
+  })
+
+exports.down = knex => knex.schema.dropTable('notes')
