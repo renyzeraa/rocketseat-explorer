59454c7c22d3624f5f914d5188fd54a8890d4818
renan <renansilvaytb@gmail.com>
2022-07-28 09:29:38 -0300

    create users command DML

---
+++ Stage08/Aula01/src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,13 @@
+const createUsers = `
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  name VARCHAR,
+  email VARCHAR,
+  password VARCHAR,
+  avatar VARCHAR NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+`
+
+module.exports = createUsers
