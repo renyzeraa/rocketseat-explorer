a26846228b42552421a46d6369b871cbb270f57b
renan <renansilvaytb@gmail.com>
2022-07-28 09:48:27 -0300

    add user ok

--- Stage08/Aula01/src/controllers/UsersController.js
+++ Stage08/Aula01/src/controllers/UsersController.js
@@ -1,14 +1,26 @@
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if (!name) {
-      throw new AppError('Nome é obrigatorio')
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUserExists) {
+      throw new AppError('Este e-mail a esta em uso')
     }
 
-    response.status(201).json({ name, email, password })
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?,?,?)',
+      [name, email, password]
+    )
+
+    return response.status(201).json()
   }
 }
 
