0a9613d8f09e55f6951a343b89e9001fb86dd0ec
renan <renansilvaytb@gmail.com>
2022-07-28 10:19:56 -0300

    fix date time and check new password

--- Stage08/Aula01/src/controllers/UsersController.js
+++ Stage08/Aula01/src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 
@@ -27,7 +27,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -49,13 +49,28 @@ class UsersController {
     user.name = name
     user.email = email
 
+    if (password && !old_password) {
+      throw new AppError('Voce precisa inserir sua senha antiga')
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     await database.run(
       `UPDATE users SET
     name = ?,
     email = ?,
-    updated_at = ?
+    password = ?,
+    updated_at = DATETIME('now')
     WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
 
     return response.json()
