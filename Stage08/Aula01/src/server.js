008e9626732563630b6199b982f82594c29c1bfa
renan <renansilvaytb@gmail.com>
2022-07-28 09:38:23 -0300

    migrations table users

--- Stage08/Aula01/src/server.js
+++ Stage08/Aula01/src/server.js
@@ -1,18 +1,17 @@
 require('express-async-errors')
-const database = require('./database/sqlite')
-
+const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 
 const express = require('express')
 
 const routes = require('./routes')
 
+migrationsRun()
+
 const app = express()
 app.use(express.json())
 
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
