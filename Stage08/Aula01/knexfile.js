72a4cdda437ddae96b1cb31c167131c6670eafb2
renan <renansilvaytb@gmail.com>
2022-07-28 10:51:42 -0300

    add migrations

--- Stage08/Aula01/knexfile.js
+++ Stage08/Aula01/knexfile.js
@@ -6,6 +6,15 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      )
+    },
 
     useNullAsDefault: true
   }
