2a00fe889df3bcf5a40138ac3c7eab5b5c5bb592
renan <renansilvaytb@gmail.com>
2022-07-28 21:13:33 -0300

    show notes

--- Stage08/Aula01/src/controllers/NotesController.js
+++ Stage08/Aula01/src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const nodemon = require('nodemon')
 const knex = require('../database/knex')
 
 class NotesController {
@@ -31,6 +32,22 @@ class NotesController {
     await knex('tags').insert(tagsInsert)
 
     response.json()
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const note = await knex('notes').where({ id }).first()
+    const tags = await knex('tags').where({ note_id: id }).orderBy('name')
+    const links = await knex('links')
+      .where({ note_id: id })
+      .orderBy('created_at')
+
+    return response.json({
+      ...note,
+      tags,
+      links
+    })
   }
 }
 
--- Stage08/Aula01/src/routes/notes.routes.js
+++ Stage08/Aula01/src/routes/notes.routes.js
@@ -7,5 +7,6 @@ const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
 
 module.exports = notesRoutes
