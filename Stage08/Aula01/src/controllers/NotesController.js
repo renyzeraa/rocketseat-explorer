965ab1d542ae5461a7fd1f5118a715679a9c7ae2
renan <renansilvaytb@gmail.com>
2022-07-28 21:23:55 -0300

    list notes

--- Stage08/Aula01/src/controllers/NotesController.js
+++ Stage08/Aula01/src/controllers/NotesController.js
@@ -57,6 +57,13 @@ class NotesController {
 
     return response.json()
   }
+
+  async index(request, response) {
+    const { user_id } = request.query
+    const notes = await knex('notes').where({ user_id }).orderBy('title')
+
+    return response.json(notes)
+  }
 }
 
 module.exports = NotesController
--- Stage08/Aula01/src/routes/notes.routes.js
+++ Stage08/Aula01/src/routes/notes.routes.js
@@ -6,6 +6,7 @@ const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+notesRoutes.get('/', notesController.index)
 notesRoutes.post('/:user_id', notesController.create)
 notesRoutes.get('/:id', notesController.show)
 notesRoutes.delete('/:id', notesController.delete)
