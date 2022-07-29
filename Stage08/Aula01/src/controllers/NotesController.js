0a870e8a581501aadb4360bec70f20f4538b2b89
renan <renansilvaytb@gmail.com>
2022-07-28 21:27:27 -0300

    where like consult

--- Stage08/Aula01/src/controllers/NotesController.js
+++ Stage08/Aula01/src/controllers/NotesController.js
@@ -59,8 +59,11 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query
-    const notes = await knex('notes').where({ user_id }).orderBy('title')
+    const { title, user_id } = request.query
+    const notes = await knex('notes')
+      .where({ user_id })
+      .whereLike('title', `%${title}%`)
+      .orderBy('title')
 
     return response.json(notes)
   }
