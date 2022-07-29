e2ffabc0dd4c0ac431dfdc54e31ef3c656942746
renan <renansilvaytb@gmail.com>
2022-07-28 21:39:14 -0300

    add filters notes

--- Stage08/Aula01/src/controllers/NotesController.js
+++ Stage08/Aula01/src/controllers/NotesController.js
@@ -66,7 +66,13 @@ class NotesController {
     if (tags) {
       const filterTags = tags.split(',').map(tag => tag.trim())
 
-      notes = await knex('tags').whereIn('name', filterTags)
+      notes = await knex('tags')
+        .select(['notes.id', 'notes.title', 'notes.user_id'])
+        .where('notes.user_id', user_id)
+        .whereLike('notes.title', `%${title}%`)
+        .whereIn('name', filterTags)
+        .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .orderBy('notes.title')
     } else {
       notes = await knex('notes')
         .where({ user_id })
