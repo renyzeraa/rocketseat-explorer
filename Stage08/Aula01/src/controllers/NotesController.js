2edc78c9cae6d86e73786275e39c9c43ae699a6f
renan <renansilvaytb@gmail.com>
2022-07-28 21:19:47 -0300

    delete notes

--- Stage08/Aula01/src/controllers/NotesController.js
+++ Stage08/Aula01/src/controllers/NotesController.js
@@ -49,6 +49,14 @@ class NotesController {
       links
     })
   }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex('notes').where({ id }).delete()
+
+    return response.json()
+  }
 }
 
 module.exports = NotesController
--- Stage08/Aula01/src/routes/notes.routes.js
+++ Stage08/Aula01/src/routes/notes.routes.js
@@ -8,5 +8,6 @@ const notesController = new NotesController()
 
 notesRoutes.post('/:user_id', notesController.create)
 notesRoutes.get('/:id', notesController.show)
+notesRoutes.delete('/:id', notesController.delete)
 
 module.exports = notesRoutes
