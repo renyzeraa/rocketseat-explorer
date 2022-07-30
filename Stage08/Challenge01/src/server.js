52a192666375c255bb3819874d9f1b979eb44b6e
renyzeraa <renansilvaytb@gmail.com>
2022-07-30 17:53:28 -0300

    server.js

--- Stage08/Challenge01/src/server.js
+++ Stage08/Challenge01/src/server.js
@@ -1,6 +1,12 @@
+const { response } = require('express')
 const express = require('express')
 
 const app = express()
+
+app.get('/message/:id/:user', (resquest, response) => {
+  const { id, user } = resquest.params
+  response.send(`Id da mensagem:${id}, para o usuario ${user} `)
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is runnig on Port ${PORT}`))
