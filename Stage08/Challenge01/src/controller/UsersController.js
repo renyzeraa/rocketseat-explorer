f1c4d67bbfddb7d3092ad8851e1ec2677a575fa5
renyzeraa <renansilvaytb@gmail.com>
2022-07-31 16:16:23 -0300

    exception handling

--- Stage08/Challenge01/src/controller/UsersController.js
+++ Stage08/Challenge01/src/controller/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if (!name) {
+      throw new AppError('Nome obrigatório')
+    }
 
     response.status(201).json({ name, email, password })
   }
--- Stage08/Challenge01/src/server.js
+++ Stage08/Challenge01/src/server.js
@@ -1,3 +1,7 @@
+require('express-async-errors') //import express async errors
+
+const AppError = require('./utils/AppError')
+
 const express = require('express')
 
 const routes = require('./routes')
@@ -7,6 +11,20 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message })
+  }
+
+  console.error(error)
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'internal server error' })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is runnig on Port ${PORT}`))
