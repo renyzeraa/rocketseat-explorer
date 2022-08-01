5bc148380680eac0ec19c0a672891a666bd987b2
renyzeraa <renansilvaytb@gmail.com>
2022-08-01 14:58:45 -0300

    show notes

--- Stage08/Challenge01/src/controller/NotesController.js
+++ Stage08/Challenge01/src/controller/NotesController.js
@@ -25,6 +25,39 @@ class NotesController {
 
     response.json()
   }
-}
 
+  async show(request, response) {
+    const { id } = request.params
+
+    const note = await knex('movie_notes').where({ id }).first()
+    const tags = await knex('movie_tags').where({ note_id: id }).orderBy('name')
+
+    return response.json({
+      ...note,
+      tags
+    })
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex('movie_notes').where({ id }).delete()
+
+    return response.json({ message: 'Note delete completed' })
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query
+    const notes = await knex('movie_notes')
+      .where({ user_id })
+      .whereLike('title', `%${title}%`)
+      .orderBy('title')
+
+    if (notes.length === 0) {
+      throw new AppError(`Movie notes not found`)
+    }
+
+    return response.json(notes)
+  }
+}
 module.exports = NotesController
--- Stage08/Challenge01/src/routes/notes.routes.js
+++ Stage08/Challenge01/src/routes/notes.routes.js
@@ -6,6 +6,9 @@ const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+notesRoutes.get('/', notesController.index)
 notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
+notesRoutes.delete('/:id', notesController.delete)
 
 module.exports = notesRoutes
