92328eafeefc92a26a3ddb7d4571289615a77c4c
renyzeraa <renansilvaytb@gmail.com>
2022-08-01 13:08:01 -0300

    fix create

--- Stage08/Challenge01/src/database/knex/migrations/20220801133104_createNotes.js
+++ Stage08/Challenge01/src/database/knex/migrations/20220801133104_createNotes.js
@@ -1,9 +1,9 @@
 exports.up = knex =>
   knex.schema.createTable('movie_notes', table => {
     table.increments('id')
-    table.text('title')
-    table.text('description')
-    table.integer('rating')
+    table.text('title').notNullable()
+    table.text('description').notNullable()
+    table.integer('rating').notNullable()
     table.integer('user_id').references('id').inTable('users')
     table.timestamp('created_at').default(knex.fn.now())
     table.timestamp('updated_at').default(knex.fn.now())
--- Stage08/Challenge01/src/database/knex/migrations/20220801133902_createTags.js
+++ Stage08/Challenge01/src/database/knex/migrations/20220801133902_createTags.js
@@ -1,9 +1,13 @@
 exports.up = knex =>
   knex.schema.createTable('movie_tags', table => {
     table.increments('id')
-    table.integer('movie_notes_id').references('id').inTable('movie_notes')
+    table
+      .integer('note_id')
+      .references('id')
+      .inTable('movie_notes')
+      .onDelete('CASCADE')
     table.integer('user_id').references('id').inTable('users')
-    table.text('name')
+    table.text('name').notNullable()
   })
 
 exports.down = knex => knex.schema.dropTable('movie_tags')
