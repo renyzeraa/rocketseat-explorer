2f300855fb6944724f62da59a2ebb522d748a36d
renyzeraa <renansilvaytb@gmail.com>
2022-08-01 18:03:42 -0300

    ✨ feat TagsController.js

--- Stage08/Challenge01/src/controller/TagsController.js
+++ Stage08/Challenge01/src/controller/TagsController.js
@@ -2,7 +2,13 @@ const AppError = require('../utils/AppError')
 const knex = require('../database/knex')
 
 class TagsController {
-  async index(request, response) {}
+  async index(request, response) {
+    const user_id = request.params
+
+    const tags = await knex('movie_tags').where({ user_id })
+
+    response.json(tags)
+  }
 }
 
 module.exports = TagsController
