c1cd34d6a4a4f77711f387009023e77af94cb650
renyzeraa <renansilvaytb@gmail.com>
2022-08-01 09:24:25 -0300

    create update user name

--- Stage08/Challenge01/src/controller/UsersController.js
+++ Stage08/Challenge01/src/controller/UsersController.js
@@ -27,6 +27,37 @@ class UsersController {
 
     return response.status(201).json()
   }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('User not found')
+    }
+
+    const userUpdateEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (userUpdateEmail && userUpdateEmail.id !== user.id) {
+      throw new AppError('Email already exists')
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(
+      `UPDATE users SET name = ?, email = ?, updated_at = ? WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
+  }
 }
 
 module.exports = UsersController
