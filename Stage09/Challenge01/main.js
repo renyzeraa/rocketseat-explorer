ee7e68af16a3c58b170fc8543f5fbb410de039de
renyzeraa <renansilvaytb@gmail.co>
2022-08-02 14:26:49 -0300

    feat header

---
+++ Stage09/Challenge01/main.js
@@ -0,0 +1,89 @@
+// String (texto)
+// Number (número)
+// if "se" (argumento "verdadeiro" ou "falso")
+
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+  showBackToTopButtonOnScroll()
+  showNavOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  const navigation = document.querySelector('#navigation')
+  if (scrollY > 1) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(
+  `#home, 
+  #home img, 
+  #home .stats, 
+  #services, 
+  #services header, 
+  #services .card' 
+  #about
+  #about header
+  #about .content
+  #contact
+  #contact header
+  #contact .content
+  #contact .button
+  `
+)
