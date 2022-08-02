c5d51fa7bd739f2ea09236bf81fb2d3dfef973b0
renyzeraa <renansilvaytb@gmail.co>
2022-08-02 11:13:33 -0300

    pull the project

--- Stage05/Aula02/main.js
+++ Stage05/Aula02/main.js
@@ -1,12 +1,16 @@
 const inputNumber = document.querySelector('.input-number')
-const randomNumber = Math.round(Math.random() * 100)
-
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 0
 
 function hitNumber() {
   document.querySelector('.container1').classList.add('hide')
   document.querySelector('.container2').classList.remove('hide')
   document.querySelector('.xAttempts').innerText = xAttempts
+}
+
+function resetPage() {
+  document.querySelector('.container1').classList.remove('hide')
+  document.querySelector('.container2').classList.add('hide')
 }
 
 function handleClick(event) {
@@ -17,4 +21,14 @@ function handleClick(event) {
   if (+inputNumber.value == randomNumber) {
     hitNumber()
   }
+}
+
+function restartGame() {
+  xAttempts = 0
+
+  const button2 = document.querySelector('.btn2')
+  button2.addEventListener('click', resetPage())
+  randomNumber = Math.round(Math.random() * 10)
+
+  return randomNumber, xAttempts
 }
