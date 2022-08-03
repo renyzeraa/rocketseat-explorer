360586c0105c0d73c4ab3cb4b65ef2a768785f92
renyzeraa <renansilvaytb@gmail.com>
2022-08-03 13:30:03 -0300

    feat about slide ok

--- Stage09/Challenge01/main.js
+++ Stage09/Challenge01/main.js
@@ -29,6 +29,46 @@ function changeHeaderWhenScroll() {
     logoHeader.setAttribute('src', './assets/imgs/logo-black.png')
   }
 }
+const count1Html = document.querySelector('.count1')
+const count2Html = document.querySelector('.count2')
+const count3Html = document.querySelector('.count3')
+
+let count1 = +count1Html.textContent
+let count2 = +count1Html.textContent
+let count3 = +count1Html.textContent
+
+function updateDisplay(count1, count2, count3) {
+  count1Html.textContent = String(count1)
+  count2Html.textContent = String(count2)
+  count3Html.textContent = String(count3)
+}
+function countdown() {
+  setTimeout(() => {
+    let count1 = +count1Html.textContent
+    let count2 = +count2Html.textContent
+    let count3 = +count3Html.textContent
+
+    if (count1 <= 19000) {
+      count1 = count1 + 1000
+    }
+
+    if (count2 <= 19) {
+      ++count2
+    }
+
+    if (count3 <= 14) {
+      ++count3
+    }
+
+    updateDisplay(count1, count2, count3)
+
+    countdown()
+  }, 100)
+}
+
+if (window.scrollY > 100) {
+  countdown()
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -99,5 +139,6 @@ function activateMenuAtCurrentSection() {
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll()
   backToTop()
+  countdown()
   // activateMenuAtCurrentSection()
 })
