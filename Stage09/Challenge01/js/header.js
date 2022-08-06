13049ec0bad5452b2ac716f6e399e86a239799ee
renyzeraa <renansilvaytb@gmail.com>
2022-08-06 18:40:34 -0300

    fix header module

--- Stage09/Challenge01/js/header.js
+++ Stage09/Challenge01/js/header.js
@@ -1,8 +1,6 @@
-import { header, navHeight } from './elements.js'
+import { header, navHeight, logoHeader } from './elements.js'
 
 export default function changeHeaderWhenScroll() {
-  const logoHeader = document.querySelector('#header nav a img')
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
     logoHeader.setAttribute('src', './assets/imgs/logo/logo-white.png')
