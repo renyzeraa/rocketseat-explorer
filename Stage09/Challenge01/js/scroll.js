a58d121d29fd8b0c3cb99aedcc882f655735e22b
renyzeraa <renansilvaytb@gmail.com>
2022-08-06 18:40:15 -0300

    feat scroll function

---
+++ Stage09/Challenge01/js/scroll.js
@@ -0,0 +1,45 @@
+import { backToTopButton } from './elements.js'
+import changeHeaderWhenScroll from './header.js'
+//modify logo menu #header
+
+export default function eventScroll() {
+  // button back to top
+  function backToTop() {
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  }
+
+  // most section menu
+  const sections = document.querySelectorAll('main section[id]')
+
+  // active animation menu
+  function activateMenuAtCurrentSection() {
+    const checkpoint = window.scrollY + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+      const sectionTop = section.offsetTop
+      const sectionHeight = section.offsetHeight
+      const sectionId = section.getAttribute('id')
+
+      const checkpointStart = checkpoint >= sectionTop
+      const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+      if (checkpointStart && checkpointEnd) {
+        document
+          .querySelector(`nav ul li a[href*='${sectionId}']`)
+          .classList.add('active')
+      } else {
+        document
+          .querySelector('nav ul li a[href*=' + sectionId + ']')
+          .classList.remove('active')
+      }
+    }
+  }
+
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+}
