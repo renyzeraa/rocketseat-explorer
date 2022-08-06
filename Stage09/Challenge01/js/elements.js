d26b84a5e447b73dbde6e24eba8cd0ca07f9e31e
renyzeraa <renansilvaytb@gmail.com>
2022-08-06 18:14:06 -0300

    fix elements module

--- Stage09/Challenge01/js/elements.js
+++ Stage09/Challenge01/js/elements.js
@@ -8,4 +8,29 @@ const count1Html = document.querySelector('.js-count1')
 const count2Html = document.querySelector('.js-count2')
 const count3Html = document.querySelector('.js-count3')
 
-export { nav, toggle, links, count1Html, count2Html, count3Html }
+// slide photos #about
+const prevButton = document.querySelector('.js-prev')
+const nextButton = document.querySelector('.js-next')
+
+//modify logo menu #header
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+// slide logos securities
+const imagesCarousel = document.getElementById('js-imgs')
+const imgCarousel = document.querySelectorAll('#js-imgs img')
+
+export {
+  nav,
+  toggle,
+  links,
+  count1Html,
+  count2Html,
+  count3Html,
+  nextButton,
+  prevButton,
+  header,
+  navHeight,
+  imagesCarousel,
+  imgCarousel
+}
