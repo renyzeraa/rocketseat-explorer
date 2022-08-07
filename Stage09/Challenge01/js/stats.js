e01496c5f04cc17dc47b580e013c62a5cdd8b9bb
renyzeraa <renansilvaytb@gmail.com>
2022-08-07 18:55:17 -0300

    fix stats module

--- Stage09/Challenge01/js/stats.js
+++ Stage09/Challenge01/js/stats.js
@@ -1,61 +1,60 @@
 import { count1Html, count2Html, count3Html } from './elements.js'
 
 // animations stats
-export default function stats() {
-  let count1 = +count1Html.textContent
-  let count2 = +count1Html.textContent
-  let count3 = +count1Html.textContent
 
-  function updateDisplay(count2, count3) {
-    count2Html.textContent = String(count2)
-    count3Html.textContent = String(count3)
-  }
+let count1 = +count1Html.textContent
+let count2 = +count1Html.textContent
+let count3 = +count1Html.textContent
 
-  function updateDisplay1(count1) {
-    count1Html.textContent = String(count1)
-  }
+function updateDisplay(count2, count3) {
+  count2Html.textContent = String(count2)
+  count3Html.textContent = String(count3)
+}
 
-  function countdown1() {
-    setTimeout(() => {
-      let count1 = +count1Html.textContent
-      if (count1 <= 9980) {
-        count1 = count1 + 20
-      }
-      updateDisplay1(count1)
-      countdown1()
-    }, 8)
-  }
+function updateDisplay1(count1) {
+  count1Html.textContent = String(count1)
+}
 
-  function countdown() {
-    setTimeout(() => {
-      let count2 = +count2Html.textContent
-      let count3 = +count3Html.textContent
+function countdown1() {
+  setTimeout(() => {
+    let count1 = +count1Html.textContent
+    if (count1 <= 9980) {
+      count1 = count1 + 20
+    }
+    updateDisplay1(count1)
+    countdown1()
+  }, 8)
+}
 
-      if (count2 <= 19) {
-        ++count2
-      }
+function countdown() {
+  setTimeout(() => {
+    let count2 = +count2Html.textContent
+    let count3 = +count3Html.textContent
 
-      if (count3 <= 14) {
-        ++count3
-      }
+    if (count2 <= 19) {
+      ++count2
+    }
 
-      updateDisplay(count2, count3)
+    if (count3 <= 14) {
+      ++count3
+    }
 
-      countdown()
-    }, 180)
-  }
+    updateDisplay(count2, count3)
 
-  if (window.innerWidth > 900) {
+    countdown()
+  }, 180)
+}
+
+if (window.innerWidth > 900) {
+  countdown()
+  countdown1()
+}
+
+function statsEvent() {
+  if ((window.scrollY = 200)) {
     countdown()
     countdown1()
   }
-
-  function eventScroll() {
-    if ((window.scrollY = 200)) {
-      countdown()
-      countdown1()
-    }
-  }
-
-  window.addEventListener('scroll', eventScroll)
 }
+
+export default statsEvent
