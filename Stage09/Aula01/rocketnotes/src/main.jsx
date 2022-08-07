56672361fe3208081259951e96326373a1391cb2
renyzeraa <renansilvaytb@gmail.com>
2022-08-07 12:59:31 -0300

    feat details

--- Stage09/Aula01/rocketnotes/src/main.jsx
+++ Stage09/Aula01/rocketnotes/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>
 )
