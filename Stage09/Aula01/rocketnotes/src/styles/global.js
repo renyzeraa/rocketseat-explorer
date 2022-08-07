1d9fab336e35a125e6164d979fc1aaf156e4b1b8
renyzeraa <renansilvaytb@gmail.com>
2022-08-07 14:30:00 -0300

    fix style font

--- Stage09/Aula01/rocketnotes/src/styles/global.js
+++ Stage09/Aula01/rocketnotes/src/styles/global.js
@@ -9,9 +9,18 @@ export default createGlobalStyle`
 
   body {
     background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
-    color: ${({ theme }) => theme.COLORS.WHITE}
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    -webkit-font-smoothing: antialiased;
+
   }
-  
+
+  body, input, button, textarea{
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+    outline: none;
+  }
+
   a {
     text-decoration: none;
   }
