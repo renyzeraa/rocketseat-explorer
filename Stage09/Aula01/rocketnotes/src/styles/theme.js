aff94bde548c122c716d52bb04429ea9c6fcc3de
renyzeraa <renansilvaytb@gmail.com>
2022-08-07 13:57:25 -0300

    feat colors theme

---
+++ Stage09/Aula01/rocketnotes/src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#232129',
+    BACKGROUND_800: '#312E38',
+    BACKGROUND_700: '#3E3B47',
+
+    WHITE: '#F4EDE8',
+    ORANGE: '#FF9000',
+
+    GRAY_100: '#999591',
+    GRAY_300: '#666360',
+
+    RED: '#FF002E'
+  }
+}
