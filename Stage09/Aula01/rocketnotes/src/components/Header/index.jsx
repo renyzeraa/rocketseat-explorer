29edbabfeb47ec87c045940f0b9babf720e25587
renyzeraa <renansilvaytb@gmail.com>
2022-08-08 14:22:54 -0300

    fix name, message HEADER

--- Stage09/Aula01/rocketnotes/src/components/Header/index.jsx
+++ Stage09/Aula01/rocketnotes/src/components/Header/index.jsx
@@ -1,5 +1,15 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/renyzeraa.png" alt="User Image" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Renan Silva</strong>
+        </div>
+      </Profile>
+    </Container>
+  )
 }
--- Stage09/Aula01/rocketnotes/src/components/Header/styles.js
+++ Stage09/Aula01/rocketnotes/src/components/Header/styles.js
@@ -13,3 +13,30 @@ export const Container = styled.header`
   justify-content: space-between;
   padding: 0 80px;
 `
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+  }
+
+  span {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  strong {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 18px;
+  }
+`
