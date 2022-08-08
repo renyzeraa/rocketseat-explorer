2680847e0924438976d8575d3c3222b5f8aef227
renyzeraa <renansilvaytb@gmail.com>
2022-08-08 14:14:35 -0300

    fix remove buttons

--- Stage09/Aula01/rocketnotes/src/pages/Details/index.jsx
+++ Stage09/Aula01/rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,13 @@
 import { Container } from './styles'
-
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details() {
   return (
     <Container>
+      <Header />
       <h1>Hello World</h1>
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
-      <Button title="Deletar" />
+      <Button title="Voltar" />
     </Container>
   )
 }
