50fc93cc6a3fa4cc7913c7e7def39a74f877f1b8
renyzeraa <renansilvaytb@gmail.com>
2022-08-08 14:15:21 -0300

    feat header style

--- Stage09/Aula01/rocketnotes/src/pages/Details/styles.js
+++ Stage09/Aula01/rocketnotes/src/pages/Details/styles.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas:
+    'header'
+    'container';
+`
