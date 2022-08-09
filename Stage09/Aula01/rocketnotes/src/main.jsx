c8ffd116ccf6d034f19cca1d032f96708b8fbe78
renyzeraa <renansilvaytb@gmail.com>
2022-08-09 14:13:02 -0300

    feat Signin Page

--- Stage09/Aula01/rocketnotes/src/main.jsx
+++ Stage09/Aula01/rocketnotes/src/main.jsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
--- Stage09/Aula01/rocketnotes/src/pages/Signin/index.jsx
+++ Stage09/Aula01/rocketnotes/src/pages/Signin/index.jsx
@@ -1,7 +1,21 @@
-import { Container } from './styles'
+import { Container, Form, Background } from './styles'
+
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
-
-export function Home() {
-  return <Container></Container>
+import { FiMail, FiLock } from 'react-icons/fi'
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+        <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
+        <Input placeholder="Senha" type="password" icon={FiLock}></Input>
+        <Button title="Login"></Button>
+        <a href="#">Criar Conta</a>
+      </Form>
+      <Background />
+    </Container>
+  )
 }
