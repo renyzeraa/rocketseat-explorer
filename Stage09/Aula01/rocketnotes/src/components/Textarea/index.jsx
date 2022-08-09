c70f2b5dac6c8f8cbb66de5f990335079d3c7e1c
renyzeraa <renansilvaytb@gmail.com>
2022-08-09 15:29:04 -0300

    feat Textare component

---
+++ Stage09/Aula01/rocketnotes/src/components/Textarea/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function Textarea({ value, ...rest }) {
+  return <Container {...rest}>{value}</Container>
+}
---
+++ Stage09/Aula01/rocketnotes/src/components/Textarea/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.textarea`
+  width: 100%;
+  height: 150px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  border: none;
+  resize: none;
+
+  margin-bottom: 8px;
+  border-radius: 10px;
+  padding: 16px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+  }
+`
