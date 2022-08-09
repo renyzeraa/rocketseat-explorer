b6562987425a270d09fd839f4a64a6a7d9585ca1
renyzeraa <renansilvaytb@gmail.com>
2022-08-09 14:56:56 -0300

    feat Profile page

--- Stage09/Aula01/rocketnotes/src/main.jsx
+++ Stage09/Aula01/rocketnotes/src/main.jsx
@@ -5,7 +5,8 @@ import GlobalStyles from './styles/global'
 // import { Details } from './pages/Details'
 // import { Home } from './pages/Home'
 // import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
+// import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 import theme from './styles/theme'
 
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
