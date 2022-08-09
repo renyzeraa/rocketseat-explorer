1af455beb42f212cec7b7fc7c38a6a7dcac8f73b
renyzeraa <renansilvaytb@gmail.com>
2022-08-09 16:04:33 -0300

    feat NewNote Page

--- Stage09/Aula01/rocketnotes/src/main.jsx
+++ Stage09/Aula01/rocketnotes/src/main.jsx
@@ -6,7 +6,7 @@ import GlobalStyles from './styles/global'
 // import { Home } from './pages/Home'
 // import { SignIn } from './pages/SignIn'
 // import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 import theme from './styles/theme'
 
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ Stage09/Aula01/rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,41 @@
+import { Container, Form } from './styles'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+import { Section } from '../../components/Section'
+import { NoteItem } from '../../components/NoteItem'
+import { Button } from '../../components/Button'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input type="text" placeholder="Título"></Input>
+          <Textarea placeholder="Observações" />
+
+          <Section title="Links Úteis">
+            <NoteItem value="link.com" />
+            <NoteItem isNew={true} placeholder="Novo Link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew={true} placeholder="Nova Tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
