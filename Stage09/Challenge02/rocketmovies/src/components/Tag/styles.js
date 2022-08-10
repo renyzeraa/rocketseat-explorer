1091483b5a07eee494de1c917ea28cee28415981
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 20:54:49 -0300

    fix tag component

--- Stage09/Challenge02/rocketmovies/src/components/Tag/styles.js
+++ Stage09/Challenge02/rocketmovies/src/components/Tag/styles.js
@@ -1,12 +1,12 @@
-import { styled } from 'styled-components'
+import styled from 'styled-components'
 
 export const Container = styled.div`
   font-size: 12px;
   font-weight: 400;
-  line-height: 14px;
 
   padding: 6px 16px;
   border-radius: 8px;
-  color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
 `
