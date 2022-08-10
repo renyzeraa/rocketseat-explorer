fa370952f7c2850b62a275576870c6479174bfc3
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 16:47:57 -0300

    feat theme style

---
+++ Stage09/Challenge02/rocketmovies/src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#1C1B1E',
+    BACKGROUND_800: '#262529',
+    BACKGROUND_700: '#3E3B47',
+    BACKGROUND_600: 'rgba(255, 133, 155, 0.05)',
+
+    WHITE: '#F4EDE8',
+    PINK: '#FF859B',
+
+    GRAY_100: '#999591',
+    GRAY_300: '#666360',
+
+    RED: '#FF002E'
+  }
+}
