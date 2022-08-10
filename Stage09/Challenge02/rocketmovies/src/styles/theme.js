df3850bfbc10782f630c1dc6e50001e444b3d706
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 16:54:08 -0300

    fix color background 600

--- Stage09/Challenge02/rocketmovies/src/styles/theme.js
+++ Stage09/Challenge02/rocketmovies/src/styles/theme.js
@@ -3,7 +3,7 @@ export default {
     BACKGROUND_900: '#1C1B1E',
     BACKGROUND_800: '#262529',
     BACKGROUND_700: '#3E3B47',
-    BACKGROUND_600: 'rgba(255, 133, 155, 0.05)',
+    BACKGROUND_600: '#48373a',
 
     WHITE: '#F4EDE8',
     PINK: '#FF859B',
