1b35d655358dc5dd297dc05cfe06862fe0d685e1
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 15:46:54 -0300

    Feat Path Pages

--- Stage09/Challenge02/rocketmovies/src/main.jsx
+++ Stage09/Challenge02/rocketmovies/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Preview } from './pages/Preview'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Preview />
+  </React.StrictMode>
 )
---
+++ Stage09/Challenge02/rocketmovies/src/pages/Preview/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles.js'
+
+export function Preview() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  )
+}
