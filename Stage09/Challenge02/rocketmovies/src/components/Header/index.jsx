8b5b2a37b4972c54a0e2099852dfab4e1987bbcd
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 19:40:15 -0300

    fix Header component

--- Stage09/Challenge02/rocketmovies/src/components/Header/index.jsx
+++ Stage09/Challenge02/rocketmovies/src/components/Header/index.jsx
@@ -4,16 +4,18 @@ import { FiSearch } from 'react-icons/fi'
 export function Header() {
   return (
     <Container>
-      <h1>
-        <a href="#">RocketMovies</a>
-      </h1>
-      <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
-      <div>
-        <div>
-          <a href="#">Renan Silva</a>
-          <a href="#">sair</a>
+      <div className="header">
+        <h1>
+          <a href="#">RocketMovies</a>
+        </h1>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+        <div className="profile">
+          <div>
+            <a href="#">Renan Silva</a>
+            <a href="#">sair</a>
+          </div>
+          <img src="https://github.com/renyzeraa.png" alt="User Image" />
         </div>
-        <img src="https://github.com/renyzeraa.png" alt="User Image" />
       </div>
     </Container>
   )
--- Stage09/Challenge02/rocketmovies/src/components/Header/styles.js
+++ Stage09/Challenge02/rocketmovies/src/components/Header/styles.js
@@ -1,53 +1,61 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
-  width: 100%;
-  height: 116px;
+  .header {
+    width: min(100%, 1120px);
+    height: 116px;
+    margin: 0 auto;
+    padding: 0 64px;
 
-  padding: 0 64px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
-
-  > h1 a {
-    color: ${({ theme }) => theme.COLORS.PINK};
-    margin-right: 32px;
-  }
-
-  > div {
     display: flex;
-    flex-direction: row;
     align-items: center;
-    align-content: center;
-    margin-left: 32px;
-    div {
-      display: grid;
-      text-align: right;
-      margin-right: 10px;
-      width: 125px;
+    justify-content: space-between;
 
-      a:first-child {
-        color: ${({ theme }) => theme.COLORS.WHITE};
-      }
-      a {
-        color: ${({ theme }) => theme.COLORS.GRAY_100};
-      }
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    > h1 a {
+      color: ${({ theme }) => theme.COLORS.PINK};
+      margin-right: 32px;
     }
 
-    img {
-      width: 64px;
-      height: 64px;
-      padding: 4px;
-      border: 1px solid #3e3b47;
-      border-radius: 50%;
-    }
+    > .profile {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      align-content: center;
 
-    img:hover {
-      transition: 0.5s;
-      transform: scale(1.1);
+      > div {
+        display: grid;
+        text-align: right;
+        margin-right: 10px;
+        width: 115px;
+
+        a:first-child {
+          color: ${({ theme }) => theme.COLORS.WHITE};
+          font-weight: 700;
+        }
+        a {
+          color: ${({ theme }) => theme.COLORS.GRAY_100};
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 18px;
+        }
+      }
+
+      img {
+        width: 64px;
+        height: 64px;
+        padding: 4px;
+        border: 1px solid #3e3b47;
+        border-radius: 50%;
+      }
+
+      img:hover {
+        transition: 0.5s;
+        transform: scale(1.1);
+      }
     }
   }
+
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
 `
