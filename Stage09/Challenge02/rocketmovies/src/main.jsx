960a527eafcc3abf9b24ada939abdfce90da58f4
renyzeraa <renansilvaytb@gmail.com>
2022-08-10 17:00:43 -0300

    feat global and theme

--- Stage09/Challenge02/rocketmovies/src/main.jsx
+++ Stage09/Challenge02/rocketmovies/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
 import { Preview } from './pages/Preview'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Preview />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Preview />
+    </ThemeProvider>
   </React.StrictMode>
 )
