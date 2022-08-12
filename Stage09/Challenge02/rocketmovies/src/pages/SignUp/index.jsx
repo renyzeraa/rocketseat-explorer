0ffed79a9d2aad18c7ef2ce025910e485e17205b
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 17:58:56 -0300

    feat SignUp page

---
+++ Stage09/Challenge02/rocketmovies/src/pages/SignUp/index.jsx
@@ -0,0 +1,27 @@
+import { Container, Form, Background } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { FiMail, FiLock, FiArrowLeft, FiUser } from 'react-icons/fi'
+
+export function SignUp() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Crie sua conta</h2>
+        <Input placeholder="Nome" type="text" icon={FiUser}></Input>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
+        <Input placeholder="Senha" type="password" icon={FiLock}></Input>
+        <Button title="Cadastrar"></Button>
+        <a to="/register">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Form>
+      <Background />
+    </Container>
+  )
+}
