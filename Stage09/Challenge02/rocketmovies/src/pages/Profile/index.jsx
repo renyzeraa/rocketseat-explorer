00a3a748c184d4767546d744a5370b2e7ccd6632
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 17:12:47 -0300

    feat Profile page

---
+++ Stage09/Challenge02/rocketmovies/src/pages/Profile/index.jsx
@@ -0,0 +1,48 @@
+import { Container, Form, Avatar } from './styles'
+import { ButtonText } from '../../components/ButtonText'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import {
+  RiArrowLeftLine,
+  RiUser2Fill,
+  RiMailLine,
+  RiLock2Line
+} from 'react-icons/ri'
+import { FiCamera } from 'react-icons/fi'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <ButtonText icon={RiArrowLeftLine} title="Voltar"></ButtonText>
+      </header>
+      <Form>
+        <Avatar>
+          <img src="https://github.com/renyzeraa.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+        <Input
+          type="text"
+          icon={RiUser2Fill}
+          placeholder="Nome Completo"
+        ></Input>
+        <Input type="text" icon={RiMailLine} placeholder="E-mail"></Input>
+        <Input
+          type="password"
+          icon={RiLock2Line}
+          placeholder="Senha atual"
+        ></Input>
+        <Input
+          type="password"
+          icon={RiLock2Line}
+          placeholder="Nova senha"
+        ></Input>
+        <Button title="Salvar"></Button>
+      </Form>
+    </Container>
+  )
+}
---
+++ Stage09/Challenge02/rocketmovies/src/pages/Profile/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+
+  > header {
+    width: 100%;
+    height: 144px;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+    display: flex;
+    align-items: center;
+
+    padding: 0 124px;
+  }
+
+  svg {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 24px;
+  }
+`
+
+export const Form = styled.form`
+  max-width: 340px;
+  margin: 30px auto;
+
+  > div:nth-child(4) {
+    margin-top: 24px;
+  }
+
+  display: grid;
+  gap: 8px;
+
+  button {
+    margin-top: 24px;
+  }
+`
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -124px auto 64px;
+  width: 186px;
+  height: 186px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    cursor: pointer;
+    bottom: 7px;
+    right: 7px;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
+
+  > label:hover {
+    transition: 0.5s;
+    transform: scale(1.2);
+  }
+`
