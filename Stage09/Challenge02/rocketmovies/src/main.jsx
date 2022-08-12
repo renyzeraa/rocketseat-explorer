34014f6e2f09d5209e0fc1fa1cb6b68d88afb49c
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 18:25:23 -0300

    feat routes index

--- Stage09/Challenge02/rocketmovies/src/main.jsx
+++ Stage09/Challenge02/rocketmovies/src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
-import { SignUp } from './pages/SignUp'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
