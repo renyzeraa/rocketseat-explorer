c9f27dd6facb412e1ec50793d368c423033490ce
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 18:25:40 -0300

    fix link to register

--- Stage09/Challenge02/rocketmovies/src/pages/SignIn/index.jsx
+++ Stage09/Challenge02/rocketmovies/src/pages/SignIn/index.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-
+import { Link } from 'react-router-dom'
 import { FiMail, FiLock } from 'react-icons/fi'
 
 export function SignIn() {
@@ -14,7 +14,7 @@ export function SignIn() {
         <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
         <Input placeholder="Senha" type="password" icon={FiLock}></Input>
         <Button title="Entrar"></Button>
-        <a to="/register">Criar Conta</a>
+        <Link to="/register">Criar Conta</Link>
       </Form>
       <Background />
     </Container>
