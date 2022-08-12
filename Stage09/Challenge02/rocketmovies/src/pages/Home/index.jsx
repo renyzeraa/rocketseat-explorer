ef65f9350be03bf84939d2a9307ba9b5b48d2613
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 16:40:44 -0300

    feat Home page

--- Stage09/Challenge02/rocketmovies/src/pages/Home/index.jsx
+++ Stage09/Challenge02/rocketmovies/src/pages/Home/index.jsx
@@ -1,5 +1,25 @@
 import { Container } from './styles'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Card } from '../../components/Card'
+
+import { FiPlus } from 'react-icons/fi'
 
 export function Home() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Header></Header>
+      <main>
+        <section className="content">
+          <h1>Meus Filmes</h1>
+          <Button title="+ Adicionar filme"></Button>
+        </section>
+        <section className="cards">
+          <Card title="Teste" />
+          <Card title="Teste" />
+          <Card title="Teste" />
+        </section>
+      </main>
+    </Container>
+  )
 }
