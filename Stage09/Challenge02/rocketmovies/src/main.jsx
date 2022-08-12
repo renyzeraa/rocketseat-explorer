7cce335fea2fcc0947a9ba4a5d5449feabcdf641
renyzeraa <renansilvaytb@gmail.com>
2022-08-12 15:39:56 -0300

    feat Home

--- Stage09/Challenge02/rocketmovies/src/main.jsx
+++ Stage09/Challenge02/rocketmovies/src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
-import { Create } from './pages/Create'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Create />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ Stage09/Challenge02/rocketmovies/src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function Home() {
+  return <Container></Container>
+}
