8ae87cf4e67dec7041a9fe45485aea7a0218c45d
renyzeraa <renansilvaytb@gmail.com>
2022-08-16 19:25:23 -0300

    fix /id param to token and fix routes

--- Stage10/Aula01/src/controllers/TagsController.js
+++ Stage10/Aula01/src/controllers/TagsController.js
@@ -2,7 +2,7 @@ const knex = require('../database/knex')
 
 class TagsController {
   async index(request, response) {
-    const { user_id } = request.params
+    const user_id = request.user.id
 
     const tags = await knex('tags').where({ user_id })
 
--- Stage10/Aula01/src/routes/tags.routes.js
+++ Stage10/Aula01/src/routes/tags.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 
 const TagsController = require('../controllers/TagsController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const tagsRoutes = Router()
 
 const tagsController = new TagsController()
 
-tagsRoutes.get('/:user_id', tagsController.index)
+tagsRoutes.get('/', ensureAuthenticated, tagsController.index)
 
 module.exports = tagsRoutes
