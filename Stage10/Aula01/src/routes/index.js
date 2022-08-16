7d5591f0e48ea044bb76efa82f69489180acca7d
renyzeraa <renansilvaytb@gmail.com>
2022-08-16 16:47:48 -0300

    fix sessions routes

--- Stage10/Aula01/src/routes/index.js
+++ Stage10/Aula01/src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express')
 const userRoutes = require('./user.routes')
 const notesRoutes = require('./notes.routes')
 const tagsRoutes = require('./tags.routes')
+const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 routes.use('/users', userRoutes)
 routes.use('/notes', notesRoutes)
 routes.use('/tags', tagsRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 module.exports = routes
