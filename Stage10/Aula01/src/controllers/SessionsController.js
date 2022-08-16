3bf820ebdc2dba70a97c3a75bfcb165ce81b1e80
renyzeraa <renansilvaytb@gmail.com>
2022-08-16 17:23:45 -0300

    fix token jwt

--- Stage10/Aula01/src/controllers/SessionsController.js
+++ Stage10/Aula01/src/controllers/SessionsController.js
@@ -1,7 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 const { compare } = require('bcryptjs')
-
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body
@@ -18,7 +19,13 @@ class SessionsController {
       throw new AppError('Email or Password incorrect', 401)
     }
 
-    return response.json(user)
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 }
 
