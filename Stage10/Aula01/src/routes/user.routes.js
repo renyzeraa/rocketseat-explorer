0def710db6e20bf2959f8d9616f84764c79489fc
renyzeraa <renansilvaytb@gmail.com>
2022-08-18 11:30:53 -0300

    feat import avatar route

--- Stage10/Aula01/src/routes/user.routes.js
+++ Stage10/Aula01/src/routes/user.routes.js
@@ -1,12 +1,26 @@
 const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file.filename)
+
+    response.json()
+  }
+)
 
 module.exports = usersRoutes
