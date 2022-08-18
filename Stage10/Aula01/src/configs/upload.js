6b33e13026b996ebcc56f43d139d366a7664daa0
renyzeraa <renansilvaytb@gmail.com>
2022-08-18 09:55:07 -0300

    feat upload imgs

---
+++ Stage10/Aula01/src/configs/upload.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const multer = require('muter')
+const crypto = require('crypto')
+
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+
+const MULTER = {
+  storage: multer.diskstorage({
+    destination: TMP_FOLDER,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const fileName = `${fileHash}-${file.orignalname}`
+    }
+  })
+}
+
+module.exports = {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MULTER
+}
