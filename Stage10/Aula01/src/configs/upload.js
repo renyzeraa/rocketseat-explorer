f4199bd312a2826c075545c31f41a5795d897272
renyzeraa <renansilvaytb@gmail.com>
2022-08-18 11:30:14 -0300

    fix Multer import

--- Stage10/Aula01/src/configs/upload.js
+++ Stage10/Aula01/src/configs/upload.js
@@ -1,12 +1,12 @@
 const path = require('path')
-const multer = require('muter')
+const multer = require('multer')
 const crypto = require('crypto')
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
 const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
 
 const MULTER = {
-  storage: multer.diskstorage({
+  storage: multer.diskStorage({
     destination: TMP_FOLDER,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex')
