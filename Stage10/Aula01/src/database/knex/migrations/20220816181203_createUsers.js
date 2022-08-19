a8f75a1d56cf9d2e51c788c33f528a8df4943173
renyzeraa <renansilvaytb@gmail.com>
2022-08-19 18:36:26 -0300

    feat table avatar

--- Stage10/Aula01/src/database/knex/migrations/20220816181203_createUsers.js
+++ Stage10/Aula01/src/database/knex/migrations/20220816181203_createUsers.js
@@ -4,6 +4,7 @@ exports.up = knex =>
     table.text('name')
     table.text('email')
     table.text('password')
+    table.string('avatar', null)
     table.timestamp('created_at').default(knex.fn.now())
     table.timestamp('updated_at').default(knex.fn.now())
   })
