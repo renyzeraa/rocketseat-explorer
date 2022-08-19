87d7836e4eeb9ddb6891bc6d42df9d162c40d785
renyzeraa <renansilvaytb@gmail.com>
2022-08-19 17:11:11 -0300

    fix fileName 🔧

--- Stage10/Aula01/src/configs/upload.js
+++ Stage10/Aula01/src/configs/upload.js
@@ -10,7 +10,7 @@ const MULTER = {
     destination: TMP_FOLDER,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex')
-      const fileName = `${fileHash}-${file.orignalname}`
+      const fileName = `${fileHash}-${file.originalname}`
 
       return callback(null, fileName)
     }
