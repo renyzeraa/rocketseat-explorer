13eec17c6c2c834f211d450b4ed0581489972c8e
renyzeraa <renansilvaytb@gmail.com>
2022-08-19 18:36:11 -0300

    feat avatar User set

--- Stage10/Aula01/src/routes/user.routes.js
+++ Stage10/Aula01/src/routes/user.routes.js
@@ -4,11 +4,13 @@ const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const UserAvatarController = require('../controllers/UserAvatarController')
 
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
@@ -16,11 +18,7 @@ usersRoutes.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  (request, response) => {
-    console.log(request.file.filename)
-
-    response.json()
-  }
+  userAvatarController.update
 )
 
 module.exports = usersRoutes
