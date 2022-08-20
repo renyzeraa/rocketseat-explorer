799abf43c200febee8452c2d7bb828dc721142ec
renyzeraa <renansilvaytb@gmail.com>
2022-08-20 15:41:55 -0300

    feat Hook Auth

---
+++ Stage10/Aula01/FrontEnd/rocketnotes/src/hooks/auth.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react'
+import { api } from '../service/api'
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+  const [data, setData] = useState({})
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post('/sessions', {
+        email,
+        password
+      })
+
+      const { user, token } = response.data
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({ user, token })
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possivel entrar')
+      }
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+
+  return context
+}
+
+export { AuthProvider, useAuth }
