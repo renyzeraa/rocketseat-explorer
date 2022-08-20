88f2ff4d61fd7fd37b153bbf567ec0182d4106c4
renyzeraa <renansilvaytb@gmail.com>
2022-08-20 16:38:44 -0300

    feat localStore user, token auth

--- Stage10/Aula01/FrontEnd/rocketnotes/src/hooks/auth.jsx
+++ Stage10/Aula01/FrontEnd/rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { api } from '../service/api'
 
 export const AuthContext = createContext({})
@@ -12,8 +12,10 @@ function AuthProvider({ children }) {
         email,
         password
       })
-
       const { user, token } = response.data
+
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+      localStorage.setItem('@rocketnotes:token', token)
 
       api.defaults.headers.authorization = `Bearer ${token}`
 
@@ -26,6 +28,17 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@rocketnotes:token')
+    const user = localStorage.getItem('@rocketnotes:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({ token, user: JSON.parse(user) })
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
