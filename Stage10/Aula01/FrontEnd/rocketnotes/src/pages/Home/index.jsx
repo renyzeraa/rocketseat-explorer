8938bf5bbdee65b751a953613a6894ff8c8cfcb0
Renan Silva <renansilvaytB@gmail.com>
2022-08-21 18:52:44 -0300

    feat tagsList in Home aside

--- Stage10/Aula01/FrontEnd/rocketnotes/src/pages/Home/index.jsx
+++ Stage10/Aula01/FrontEnd/rocketnotes/src/pages/Home/index.jsx
@@ -1,11 +1,24 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 import { FiPlus } from 'react-icons/fi'
+import { api } from '../../service/api'
 export function Home() {
+  const [tags, setTags] = useState([])
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/tags')
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return (
     <Container>
       <Brand>
@@ -15,14 +28,14 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive></ButtonText>
+          <ButtonText title={'Todos'} isActive></ButtonText>
         </li>
-        <li>
-          <ButtonText title="Node"></ButtonText>
-        </li>
-        <li>
-          <ButtonText title="React"></ButtonText>
-        </li>
+        {tags &&
+          tags.map(tag => (
+            <li key={tag.id}>
+              <ButtonText title={tag.name}></ButtonText>
+            </li>
+          ))}
       </Menu>
 
       <Search>
