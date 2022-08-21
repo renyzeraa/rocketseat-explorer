313ffd1fad2714963c4a195a00969d8603697881
Renan Silva <renansilvaytB@gmail.com>
2022-08-21 20:46:44 -0300

    fix bug profile Img

--- Stage10/Aula01/FrontEnd/rocketnotes/src/pages/Profile/index.jsx
+++ Stage10/Aula01/FrontEnd/rocketnotes/src/pages/Profile/index.jsx
@@ -28,14 +28,16 @@ export function Profile() {
   const [avatarFile, setAvatarFile] = useState(null)
 
   async function handleUpdate() {
-    const user = {
+    const updated = {
       name,
       email,
       password: passwordNew,
       old_password: passwordOld
     }
 
-    await updateProfile({ user, avatarFile })
+    const userUpdated = Object.assign(user, updated)
+
+    await updateProfile({ user: userUpdated, avatarFile })
   }
 
   function handleChangeAvatar(event) {
