6608e766d9c759c71e4317c4f7634ff6178a1b57
Renan Silva <renansilvaytB@gmail.com>
2022-08-21 18:54:25 -0300

    fix groupby name tags at home

--- Stage10/Aula01/BackEnd/src/controllers/TagsController.js
+++ Stage10/Aula01/BackEnd/src/controllers/TagsController.js
@@ -4,7 +4,7 @@ class TagsController {
   async index(request, response) {
     const user_id = request.user.id
 
-    const tags = await knex('tags').where({ user_id })
+    const tags = await knex('tags').where({ user_id }).groupBy('name')
 
     return response.json(tags)
   }
