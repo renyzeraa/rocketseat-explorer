7679ee2943c218927969f41e63ff2230ef80c2cf
Renan Silva <renansilvaytB@gmail.com>
2022-08-21 19:39:52 -0300

    fix GroupBy set notes in Home page

--- Stage10/Aula01/BackEnd/src/controllers/NotesController.js
+++ Stage10/Aula01/BackEnd/src/controllers/NotesController.js
@@ -72,6 +72,7 @@ class NotesController {
         .whereLike('notes.title', `%${title}%`)
         .whereIn('name', filterTags)
         .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .groupBy('notes.id')
         .orderBy('notes.title')
     } else {
       notes = await knex('notes')
