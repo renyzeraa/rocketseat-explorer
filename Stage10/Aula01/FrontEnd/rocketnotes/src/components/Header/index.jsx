dd248ec31df575d310f910e9ad43f02a27bd4aa7
Renan Silva <renansilvaytB@gmail.com>
2022-08-21 17:00:25 -0300

    fix newavatar

--- Stage10/Aula01/FrontEnd/rocketnotes/src/components/Header/index.jsx
+++ Stage10/Aula01/FrontEnd/rocketnotes/src/components/Header/index.jsx
@@ -1,17 +1,23 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 import { useAuth } from '../../hooks/auth'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { api } from '../../service/api'
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { user, signOut } = useAuth()
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/renyzeraa.png" alt="User Image" />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem-vindo</span>
-          <strong>Renan Silva</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
