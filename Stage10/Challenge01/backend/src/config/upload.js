8e420114dde25da8d0eb121d8a6219e5ab8b0f1f
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 15:04:09 -0300

    fix Upload folder directory

--- Stage10/Challenge01/backend/src/config/upload.js
+++ Stage10/Challenge01/backend/src/config/upload.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 const crypto = require('crypto')
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
-const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
 
 const MULTER = {
   storage: multer.diskStorage({
--- Stage10/Challenge01/backend/src/routes/user.routes.js
+++ Stage10/Challenge01/backend/src/routes/user.routes.js
@@ -1,13 +1,24 @@
-const { Router } = require('express')
+const { Router, response } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../config/upload')
 
 const UsersController = require('../controller/UsersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
-
+const upload = multer(uploadConfig.MULTER)
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (req, res) => {
+    console.log(req.file.filename)
+    res.json()
+  }
+)
 
 module.exports = usersRoutes
