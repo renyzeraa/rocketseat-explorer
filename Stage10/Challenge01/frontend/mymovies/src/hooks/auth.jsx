659a04733645530ef0a861934c3b49399324a801
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 17:39:36 -0300

    feat Authentication Control

--- Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
@@ -1,10 +1,29 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState } from 'react'
+import { api } from '../service/api'
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState('')
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post('/sessions', { email, password })
+      const { user, token } = response.data
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({ user, token })
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possível entrar')
+      }
+    }
+  }
   return (
-    <AuthContext.Provider value={{ name: 'renan', email: 'renan@email.com' }}>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
   )
--- Stage10/Challenge01/frontend/mymovies/src/pages/SignIn/index.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
@@ -6,18 +7,34 @@ import { FiMail, FiLock } from 'react-icons/fi'
 import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
-  const data = useAuth()
-  console.log(data)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
 
   return (
     <Container>
       <Form>
-        <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
-        <h2>Faça seu login</h2>
-        <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
-        <Input placeholder="Senha" type="password" icon={FiLock}></Input>
-        <Button title="Entrar"></Button>
+        <h1>My Movies</h1>
+        <p>Aplicação para salvar seus filmes favoritos 🎬👌🏼</p>
+        <h2>Faça seu login 👇</h2>
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        ></Input>
+        <Button title="Entrar" onClick={handleSignIn}></Button>
         <Link to="/register">Criar Conta</Link>
       </Form>
       <Background />
