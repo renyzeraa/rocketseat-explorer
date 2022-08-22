e261cdbf8f5d6ead5d7b38b03d75bb2efa76b2b9
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 17:22:49 -0300

    feat AuthProvider

--- Stage10/Challenge01/frontend/mymovies/src/main.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
+import { AuthProvider } from './hooks/auth'
 
 import { Routes } from './routes'
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
