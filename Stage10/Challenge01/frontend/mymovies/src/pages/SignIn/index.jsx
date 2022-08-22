789bc892f00535727f6b452cc2ba7d7efc5cddb9
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 17:22:38 -0300

    fix tests context

--- Stage10/Challenge01/frontend/mymovies/src/pages/SignIn/index.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/pages/SignIn/index.jsx
@@ -3,8 +3,12 @@ import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
 import { FiMail, FiLock } from 'react-icons/fi'
+import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
+  const data = useAuth()
+  console.log(data)
+
   return (
     <Container>
       <Form>
