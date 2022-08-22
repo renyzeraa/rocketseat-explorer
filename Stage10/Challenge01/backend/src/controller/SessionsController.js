35de7d3c4d82d9a29abfc8b8fd7fab9ceedb1337
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 13:22:33 -0300

    fix check password

--- Stage10/Challenge01/backend/src/controller/SessionsController.js
+++ Stage10/Challenge01/backend/src/controller/SessionsController.js
@@ -1,6 +1,6 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
-
+const { compare } = require('bcryptjs')
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body
@@ -8,6 +8,11 @@ class SessionsController {
     const user = await knex('users').where({ email }).first()
 
     if (!user) {
+      throw new AppError('Email e/ou senha incorreta', 401)
+    }
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
       throw new AppError('Email e/ou senha incorreta', 401)
     }
 
