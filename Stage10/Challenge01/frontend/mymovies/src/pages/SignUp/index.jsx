f550eca6cb729195bc2c034a00a124deff61e243
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 17:22:05 -0300

    fix navigate

--- Stage10/Challenge01/frontend/mymovies/src/pages/SignUp/index.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/pages/SignUp/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { api } from '../../service/api'
 import { FiMail, FiLock, FiArrowLeft, FiUser } from 'react-icons/fi'
 
@@ -10,6 +10,8 @@ export function SignUp() {
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
 
   function handleSignUp() {
     if (!name || !email || !password) {
@@ -20,6 +22,7 @@ export function SignUp() {
       .post('/users', { name, email, password })
       .then(() => {
         alert('Usuário cadastrado com sucesso')
+        navigate(-1)
       })
       .catch(error => {
         if (error.response) {
@@ -57,10 +60,14 @@ export function SignUp() {
         ></Input>
 
         <Button title="Cadastrar" onClick={handleSignUp}></Button>
-        <Link to="/">
+        <a
+          onClick={() => {
+            navigate(-1)
+          }}
+        >
           <FiArrowLeft />
           Voltar para o login
-        </Link>
+        </a>
       </Form>
       <Background />
     </Container>
