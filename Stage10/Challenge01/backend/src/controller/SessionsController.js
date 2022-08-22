b77c616af1c83a08747ffa47edce259ae0566522
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 13:19:22 -0300

    fix check user email

--- Stage10/Challenge01/backend/src/controller/SessionsController.js
+++ Stage10/Challenge01/backend/src/controller/SessionsController.js
@@ -1,7 +1,17 @@
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body
-    return response.json({ email, password })
+
+    const user = await knex('users').where({ email }).first()
+
+    if (!user) {
+      throw new AppError('Email e/ou senha incorreta', 401)
+    }
+
+    return response.json({ user })
   }
 }
 
