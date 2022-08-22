23c143bd31af1cde4f06829f6540371e9488836a
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 13:57:35 -0300

    feat middleware in user.routes

--- Stage10/Challenge01/backend/src/routes/user.routes.js
+++ Stage10/Challenge01/backend/src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 
 const UsersController = require('../controller/UsersController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
 
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
-usersRoutes.put('/:id', usersController.update)
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
 
 module.exports = usersRoutes
