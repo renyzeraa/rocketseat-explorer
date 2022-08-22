5930d6b8a4f5bb43944947ae27827aaa19aea24e
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 14:12:00 -0300

    fix ensureAuthenticated tags.routes

--- Stage10/Challenge01/backend/src/controller/TagsController.js
+++ Stage10/Challenge01/backend/src/controller/TagsController.js
@@ -3,7 +3,7 @@ const knex = require('../database/knex')
 
 class TagsController {
   async index(request, response) {
-    const { user_id } = request.params
+    const user_id = request.user.id
 
     const tags = await knex('movie_tags').where({ user_id })
 
--- Stage10/Challenge01/backend/src/routes/tags.routes.js
+++ Stage10/Challenge01/backend/src/routes/tags.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
 
 const TagsController = require('../controller/TagsController')
-
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 const tagsRoutes = Router()
 
 const tagsController = new TagsController()
 
-tagsRoutes.get('/:user_id', tagsController.index)
+tagsRoutes.get('/', ensureAuthenticated, tagsController.index)
 
 module.exports = tagsRoutes
