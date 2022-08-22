bae99883b80501a6dab25dd435e8c16065668fc9
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 19:30:43 -0300

    feat localStorage user and token auth

--- Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { api } from '../service/api'
 
 export const AuthContext = createContext({})
@@ -11,6 +11,9 @@ function AuthProvider({ children }) {
       const response = await api.post('/sessions', { email, password })
       const { user, token } = response.data
 
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+      localStorage.setItem('@rocketnotes:token', token)
+
       api.defaults.headers.authorization = `Bearer ${token}`
 
       setData({ user, token })
@@ -22,6 +25,21 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@rocketnotes:token')
+    const user = localStorage.getItem('@rocketnotes:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
+
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
