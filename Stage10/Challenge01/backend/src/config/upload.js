b97d3d00ba46581e4d0f41a2fdb9210ecb5c3ead
Renan Silva <renansilvaytB@gmail.com>
2022-08-22 14:44:33 -0300

    feat Upload Img

---
+++ Stage10/Challenge01/backend/src/config/upload.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const multer = require('multer')
+const crypto = require('crypto')
+
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename: (req, file, cb) => {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
+
+      return cb(null, fileName)
+    }
+  })
+}
+
+module.exports = {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MULTER
+}
