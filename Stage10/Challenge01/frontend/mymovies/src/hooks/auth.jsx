c68fd822416cbfa8e61b7ea0c62fbd4581c7fb79
Renan Silva <renansilvaytB@gmail.com>
2022-08-23 11:57:08 -0300

    feat updateProfile function

--- Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
@@ -32,6 +32,23 @@ function AuthProvider({ children }) {
     setData({})
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put('/users', user)
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+
+      setData({ user, token: data.token })
+
+      alert('Dados alterados com sucesso!')
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possível atualizar o perfil ')
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('@rocketnotes:token')
     const user = localStorage.getItem('@rocketnotes:user')
@@ -47,7 +64,9 @@ function AuthProvider({ children }) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user, signOut }}>
+    <AuthContext.Provider
+      value={{ signIn, signOut, updateProfile, user: data.user }}
+    >
       {children}
     </AuthContext.Provider>
   )
