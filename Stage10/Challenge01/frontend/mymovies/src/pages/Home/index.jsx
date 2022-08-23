a1117578da29bc18b72d1e069dab88f754ea8ae2
Renan Silva <renansilvaytB@gmail.com>
2022-08-23 15:17:14 -0300

    fix Home most notes

--- Stage10/Challenge01/frontend/mymovies/src/pages/Home/index.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/pages/Home/index.jsx
@@ -3,13 +3,31 @@ import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Card } from '../../components/Card'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { api } from '../../service/api'
 
 export function Home() {
   const navigate = useNavigate()
 
+  const [notes, setNotes] = useState([])
+  const [search, setSearch] = useState([])
+
+  function handleMoviePreview(id) {
+    navigate(`/preview/${id}`)
+  }
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/notes?title=${search}`)
+      const notes = response.data
+      setNotes(notes)
+    }
+    fetchNotes()
+  }, [search])
+
   return (
     <Container>
-      <Header></Header>
+      <Header onChange={e => setSearch(e.target.value)}></Header>
       <main>
         <section className="content">
           <h1>Meus Filmes Favoritos ✨</h1>
@@ -18,9 +36,14 @@ export function Home() {
           </a>
         </section>
         <section className="cards">
-          <Card title="Teste" />
-          <Card title="Teste" />
-          <Card title="Teste" />
+          {notes &&
+            notes.map(note => (
+              <Card
+                key={note.id}
+                data={note}
+                onClick={() => handleMoviePreview(note.id)}
+              />
+            ))}
         </section>
       </main>
     </Container>
