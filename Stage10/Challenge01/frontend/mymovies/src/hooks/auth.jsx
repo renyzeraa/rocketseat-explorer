8f87d14a18f366e5cc6761f80011507899ac53c1
Renan Silva <renansilvaytB@gmail.com>
2022-08-23 12:35:19 -0300

    fix updateProfile Avatar

--- Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/hooks/auth.jsx
@@ -32,8 +32,17 @@ function AuthProvider({ children }) {
     setData({})
   }
 
-  async function updateProfile({ user }) {
+  async function updateProfile({ user, avatarFile }) {
     try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData()
+        fileUploadForm.append('avatar', avatarFile)
+
+        const response = await api.patch('/users/avatar', fileUploadForm)
+
+        user.avatar = response.data.avatar
+      }
+
       await api.put('/users', user)
       localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
 
