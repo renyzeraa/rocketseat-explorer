43a924f97594dee2ea3851d43c98127d85519604
Renan Silva <renansilvaytB@gmail.com>
2022-08-23 15:32:32 -0300

    fix id route params

--- Stage10/Challenge01/frontend/mymovies/src/routes/app.routes.jsx
+++ Stage10/Challenge01/frontend/mymovies/src/routes/app.routes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-
 import { Create } from '../pages/Create'
 import { Home } from '../pages/Home'
 import { Profile } from '../pages/Profile'
@@ -11,7 +10,7 @@ export function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/create" element={<Create />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/preview" element={<Preview />} />
+      <Route path="/preview/:id" element={<Preview />} />
     </Routes>
   )
 }
