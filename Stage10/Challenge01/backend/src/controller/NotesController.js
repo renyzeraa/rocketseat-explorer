1a969bd00282c1224387906c468b7642a838164f
Renan Silva <renansilvaytB@gmail.com>
2022-08-23 18:03:37 -0300

    fix Notescontroller

--- Stage10/Challenge01/backend/src/controller/NotesController.js
+++ Stage10/Challenge01/backend/src/controller/NotesController.js
@@ -29,12 +29,16 @@ class NotesController {
   async show(request, response) {
     const { id } = request.params
 
-    const note = await knex('movie_notes').where({ id }).first()
-    const tags = await knex('movie_tags').where({ note_id: id }).orderBy('name')
+    const note = await knex('notes').where({ id }).first()
+    const tags = await knex('tags').where({ note_id: id }).orderBy('name')
+    const links = await knex('links')
+      .where({ note_id: id })
+      .orderBy('created_at')
 
     return response.json({
       ...note,
-      tags
+      tags,
+      links
     })
   }
 
