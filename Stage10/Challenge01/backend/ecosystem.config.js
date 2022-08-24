ee81cb0a1a276813bcc2e4ad32574bf0a7c7f98d
Renan Silva <renansilvaytB@gmail.com>
2022-08-24 15:50:36 -0300

    fix ecosystem

--- Stage10/Challenge01/backend/ecosystem.config.js
+++ Stage10/Challenge01/backend/ecosystem.config.js
@@ -3,16 +3,13 @@ module.exports = {
     {
       name: 'app',
       script: './src/server.js',
+      instances: 'max',
       env: {
         NODE_ENV: 'development'
       },
       env_production: {
         NODE_ENV: 'production'
       }
-    },
-    {
-      name: 'worker',
-      script: 'worker.js'
     }
   ]
 }
